3d6fad84b5f300b6a18698080dfcfc99a53c60ed
Dibo72 <humanida133@gmail.com>
2026-02-27 08:57:10 +0100

    Ejercicio acabado

--- src/NoRepetido.java
+++ src/NoRepetido.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+public class NoRepetido {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        boolean repetido = false;
+        int posicion = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Introduce la longitud del array:");
+        int longitud = sc.nextInt();
+
+        int[] primero = new int[longitud];
+        int[] SinDuplicados = new int[longitud];
+
+        for (int i = 0; i < longitud; i++) {
+            System.out.println("Introduce el valor numero " +  (i+1) + ":");
+            primero[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < longitud; i++) {
+            repetido = false;
+            for (int j = 0; j < longitud; j++) {
+                if (j!=i && primero[i]==SinDuplicados[j]) {
+                    repetido = true;
+                }
+            }
+            if (!repetido) {
+                SinDuplicados[posicion] = primero[i];
+                posicion++;
+            }
+        }
+
+        System.out.println("Array sin repetidos:");
+        for (int i = 0; i < posicion; i++) {
+            System.out.print(SinDuplicados[i] + " ");
         }
     }
 }
